3ae3b9ce29aba9ae4d295c04cbb90b3cf6a6aded
Mischa <GerasimowMichail@yandex.ru>
2022-10-31 23:23:10 +0300

    победил 13 задачу

--- z2/Program.cs
+++ z2/Program.cs
@@ -7,15 +7,34 @@
 
 // 32679 -> 6
 Console.WriteLine("ВВедите число: ");
-int x=Convert.ToInt32 ( Console.ReadLine());
-
-int n = x, k = 0, i = 0;
-for(; n > 10; i ++)
+int x = Convert.ToInt32(Console.ReadLine());
+int y = x / 100;
+int i = 1;
+int j = 1;
+int q = 1;
+int z = 10;
+int res;
+if ((x / 10) > 10)
 {
-k = n % 10; //Последнее число из n
-n =n/10;
-Console.WriteLine("{0}", k);
+    Console.WriteLine($"Введенное число {x} трехзначное или больше");
+    while ((x / i) > 10)
+    {
+        i = i * 10;
+        j += 1;
+    }
+
+    while (q < (j - 2))
+    {
+        q = q + 1;
+        z = z * 10;
+
+    }   
+    res = ((x % z) / (z / 10));
+    Console.WriteLine($"Третье число слева {res} ");
+
 }
-k = n;
-Console.WriteLine("{0}", k);
-Console.WriteLine("Число разрядов {0}", ++i);
+
+else
+    Console.WriteLine("третьей цифры нет");
+
+
