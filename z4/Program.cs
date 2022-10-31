aad65bfcb2dc1c9e8bd2ac08246611487dea46b2
Mischa <GerasimowMichail@yandex.ru>
2022-11-01 00:20:07 +0300

    я решил 13 задачу

--- z4/Program.cs
+++ z4/Program.cs
@@ -10,6 +10,7 @@ int j = 1;
 int q = 1;
 int z = 10;
 int res;
+int res1;
 if ((x / 10) > 10)
 {
     Console.WriteLine($"Введенное число {x} трехзначное или больше");
@@ -19,14 +20,14 @@ if ((x / 10) > 10)
         j += 1;
     }
 
-    while (q < (j - 1))
+    while (q < (j ))
     {
         q = q + 1;
         z = z * 10;
 
     }   
     res = ((x % z) / (z / 10));
-    Console.WriteLine($"Третье число слева {res} ");
+    Console.WriteLine($"первое число слева {res}" +  ((x % (z/100)) ) );
 
 }
 
